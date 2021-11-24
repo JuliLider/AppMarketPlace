0ac5b50f3c2a31b7ece5463835eeb05c2b71f527
camilogarzon16 <camilo.garzonl@outlook.com>
2021-11-24 13:35:34 -0500

    commit6

--- app/src/main/java/com/example/appmarketplace/WelcomeActivity.kt
+++ app/src/main/java/com/example/appmarketplace/WelcomeActivity.kt
@@ -6,12 +6,23 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var appbarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
         val fab: View = findViewById(R.id.fab)
@@ -20,12 +31,32 @@ class WelcomeActivity : AppCompatActivity() {
             Snackbar.make(view, R.string.text_fab, Snackbar.LENGTH_LONG).show()
         }
 
+        val drawerLayout:DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView:NavigationView = findViewById(R.id.nav_view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appbarConfiguration = AppBarConfiguration(setOf(R.id.nav_todo, R.id.nav_about), drawerLayout)
+
+        setupActionBarWithNavController(navController, appbarConfiguration)
+
+        navView.setupWithNavController(navController)
+
+
         if (savedInstanceState == null){
             supportFragmentManager.beginTransaction()
                 .setReorderingAllowed(true)
                 .add(R.id.fragment_container_view, ToDoFragment::class.java, null, "todo")
                 .commit()
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        return navController.navigateUp(appbarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
